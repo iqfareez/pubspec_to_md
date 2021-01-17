86a1516a892222ece409ba0e92cccf2f9a883119
iqfareez <iqmal3@outlook.com>
2021-01-17 16:50:34 +0800

    yup works

--- scratch.dart
+++ scratch.dart
@@ -1,15 +1,16 @@
 // readme generator
 
 const baseUrl = 'https://pub.dev/packages/';
-var urls = '''  font_awesome_flutter : 150.70.125.246
-  url_launcher : 150.70.125.246 
-  cupertino_icons : 113.18.129.172
-  auto_size_text : 184.133.215.213
-  flutter_neumorphic : 125.92.244.104
-  path_provider : 195.238.41.151
-  share : 69.86.200.216
-  keyboard_avoider : 112.125.85.112
-  qr_flutter : 171.249.1.103 
+//TODO: Buat video recording
+var urls = '''  font_awesome_flutter : 
+  url_launcher : 
+  cupertino_icons : 
+  auto_size_text : 
+  flutter_neumorphic : 
+  path_provider : 
+  share : 
+  keyboard_avoider : 
+  qr_flutter : 
 
   '''; //imagine ip adddress as version number
 
@@ -36,10 +37,11 @@ void main() {
 
   //printing parallel array
   for (var i = 0; i < packageName.length; i++) {
-    print(i + 1);
+    print('${i + 1}: ${packageName[i]} | ${packageVersion[i]}');
     //TODO: test by printing each element in paralle array
-    print('\n');
+
   }
+  print('\n');
 
   // for (var item in trimmedUrl2) {
   //   var fullUrl = baseUrl + item;
