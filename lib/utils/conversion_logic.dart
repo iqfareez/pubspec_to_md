09abb94f457a76afac115e40afc6b174aabab610
iqfareez <iqmal3@outlook.com>
2021-01-18 13:29:26 +0800

    fuhh to test nnti 10 min bvefore zohor huhu

--- lib/utils/conversion_logic.dart
+++ lib/utils/conversion_logic.dart
@@ -3,26 +3,28 @@ class Conversion {
   var baseUrl = 'https://pub.dev/packages/';
 
   String convertFormattedMd(String input) {
-    String fullUrl = '';
-    var urls = input;
-    var trimmedUrl = urls.trim(); //remove any unwanted lines
+    var trimmedUrl = input.trim(); //remove any unwanted lines
     var splittedUrl = trimmedUrl.split('\n'); //transform into a list
-    var trimmedUrl2 = [];
+    var output = '';
+
+    //parallel array
+    List packageName = [];
+    List packageVersion = [];
 
     for (var item in splittedUrl) {
-      var temp = item.trim();
-      trimmedUrl2.add(temp); //add each trimmedUrl into new list
+      var temp = item
+          .trim(); //remove any leading or trailing whitespaces in each element
+      // https://stackoverflow.com/questions/60402195/flutter-dart-split-string-by-first-occurrence
+      int idx = item.indexOf(":");
+      packageName.add(item.substring(0, idx).trim());
+      packageVersion.add(item.substring(idx + 1).trim());
+    }
+    for (var item in packageName) {
+      var fullUrl = baseUrl + item;
+      output = output + fullUrl;
     }
 
-    for (var item in trimmedUrl2) {
-      fullUrl = fullUrl + baseUrl + item + '\n';
-    }
-    print(fullUrl);
-    // https://stackoverflow.com/questions/60402195/flutter-dart-split-string-by-first-occurrence
-    return fullUrl;
+    return output;
+    //TODO: need to test
   }
-
-  // void main() {
-
-  // }
 }
--- scratch.dart
+++ scratch.dart
@@ -1,23 +1,23 @@
 // readme generator
 
 const baseUrl = 'https://pub.dev/packages/';
-//TODO: Buat video recording
-var urls = '''  font_awesome_flutter : 
-  url_launcher : 
-  cupertino_icons : 
-  auto_size_text : 
-  flutter_neumorphic : 
-  path_provider : 
-  share : 
-  keyboard_avoider : 
-  qr_flutter : 
+
+var urls = '''  
+font_awesome_flutter : 99.40.106.56 
+  url_launcher : 11.33.201.185 
+  cupertino_icons : 14.96.49.214 
+  auto_size_text : 253.217.163.175 
+  flutter_neumorphic : 115.166.247.30 
+  path_provider : 147.52.231.161 
+  share : 220.8.167.137 
+  keyboard_avoider : 89.140.238.82 
+  qr_flutter : 159.17.239.244 
 
   '''; //imagine ip adddress as version number
 
 void main() {
   var trimmedUrl = urls.trim(); //remove any unwanted lines
   var splittedUrl = trimmedUrl.split('\n'); //transform into a list
-  var trimmedUrl2 = [];
 
   //parallel array
   List packageName = [];
@@ -28,23 +28,27 @@ void main() {
         .trim(); //remove any leading or trailing whitespaces in each element
     // https://stackoverflow.com/questions/60402195/flutter-dart-split-string-by-first-occurrence
     int idx = item.indexOf(":");
-    // print(idx);
     packageName.add(item.substring(0, idx).trim());
     packageVersion.add(item.substring(idx + 1).trim());
-
-    // trimmedUrl2.add(temp); //add each trimmedUrl into new list
   }
 
   //printing parallel array
-  for (var i = 0; i < packageName.length; i++) {
-    print('${i + 1}: ${packageName[i]} | ${packageVersion[i]}');
-    //TODO: test by printing each element in paralle array
+  // for (var i = 0; i < packageName.length; i++) {
+  //   print('${i + 1}: ${packageName[i]} | ${packageVersion[i]}');
+  // }
 
+  //Adding link to each url
+  //VIEW OPTION 1 (URL ONLY)
+  // for (var item in packageName) {
+  //   var fullUrl = baseUrl + item;
+  //   print(fullUrl);
+  // }
+
+  //VIEW OPTION 2 (URL & VERSION)
+  for (var i = 0; i < packageName.length; i++) {
+    var fullUrl = baseUrl + packageName[i];
+    var output = fullUrl + ' (Version: ${packageVersion[i]})';
+    print(output);
   }
   print('\n');
-
-  // for (var item in trimmedUrl2) {
-  //   var fullUrl = baseUrl + item;
-  //   // print(fullUrl);
-  // }
 }
